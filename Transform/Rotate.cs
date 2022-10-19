6886305795cb19a02c5778eb0db27a8255cff65e
iluvgirlswithglasses <luunamdat204@gmail.com>
2022-10-19 13:58:35 +0700

    i'm about to slaughter Affine algorithm

---
+++ Transform/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmguCvDoodle.Transform
+{
+    /*
+    references:
+
+    https://en.wikipedia.org/wiki/Affine_transformation
+    https://www.algorithm-archive.org/contents/affine_transformations/affine_transformations.html
+    */
+    
+    class Rotate
+    {
+
+    }
+}
